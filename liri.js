b8b5d47333fd93e131dc0c77605a298c7ddebda2
JBrozinski <joseph.brozinski@gmail.com>
2020-04-08 15:13:48 -0600

    got the spotify api set up

--- liri.js
+++ liri.js
@@ -3,6 +3,8 @@ var axios = require("axios");
 var Spotify = require("node-spotify-api");
 var keys = require("./key.js");
 var spotify = new Spotify(keys.spotify);
+// var concert = new Concert();
+// var Concert = require();
 
 var command = process.argv[2];
 var search = process.argv.slice(3).join(" ");
@@ -10,7 +12,41 @@ var search = process.argv.slice(3).join(" ");
 console.log(command, ":", search);
 
 if (command === "spotify-this-song") {
-  spotify.search({ type: "track", query: search }, function(err, data) {
+  spotify.search({ type: "track", query: search }, function (err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+
+    console.log(data.tracks);
+    console.log(
+      (url =
+        "https://rest.bandsintown.com/artists/weezer/events?app_id=codingbootcamp")
+    );
+  });
+}
+
+if (command === "concert-this") {
+  concert.search({ type: "track", query: search }, function (err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+
+    console.log(data.tracks);
+  });
+}
+
+if (command === "movie-this") {
+  spotify.search({ type: "track", query: search }, function (err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+
+    console.log(data);
+  });
+}
+
+if (command === "do-what-it-says") {
+  spotify.search({ type: "track", query: search }, function (err, data) {
     if (err) {
       return console.log("Error occurred: " + err);
     }
